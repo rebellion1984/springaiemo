718e9587980d8da463e2bedcdeaea9cd6ad86864
rebellion1984 <robel_beyene84@yahoo.co.uk>
2026-06-12 00:41:34 +0100

    initial

---
+++ src/main/java/com/example/springaiemo/SpringaiemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springaiemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaiemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringaiemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springaiemo/SpringaiemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springaiemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringaiemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
