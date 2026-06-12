f7a20a4986dcd4bd2b12e373cd193a01e1b159c1
rebellion1984 <robel_beyene84@yahoo.co.uk>
2026-06-12 14:35:16 +0100

    ping ollama and get response

--- src/main/java/com/example/springaiemo/OllamaController.java
+++ src/main/java/com/example/springaiemo/OllamaController.java
@@ -1,12 +1,41 @@
 package com.example.springaiemo;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/api/ollama")
-@CrossOrigin("*")
 public class OllamaController {
-    
+
+    private ChatClient chatClient;
+
+    public OllamaController(OllamaChatModel chatClient) {
+        this.chatClient = ChatClient.create(chatClient);
+    }
+
+    @GetMapping("/{message}")
+    public ResponseEntity<String> getAnswer(@PathVariable String message) {
+        ChatResponse chatResponse = chatClient
+                .prompt(message)
+                .call()
+                .chatResponse();
+
+        System.out.println(chatResponse.getMetadata().getModel());
+
+        String response = chatResponse.getResult().getOutput().getText();
+
+        System.out.println(response);
+
+        return ResponseEntity.ok(response);
+    }
+
+    //url http://localhost:8080/api/ollama/{message}
+
 }
